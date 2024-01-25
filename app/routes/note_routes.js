e2424e0091a83780e5bd7a5d84517cbb05da1765
Kevin Langat <kevin.langat@nathandigital.com>
2024-01-25 17:36:16 +0300

    create project skeleton, add required files

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,1 +1,1 @@
-module.exports = function (app, db) { app.post('/notes', (req, res) => { res.send('Hello') }); };  // You'll create your note here. 
+module.exports = function (app, db) { app.post('/notes', (req, res) => { console.log(req.body), res.send('Hello') }); };  // You'll create your note here. 
--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express'); const MongoClient = require('mongodb').MongoClient; const bodyParser = require('body-parser');
 const app = express();
 const port = 3300;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/status", (request, response) => {
+    const status = {
+       "Status": "Running"
+    };
+    
+    response.send(status);
+ });
 require('./app/routes')(app, {}); app.listen(port, () => { console.log('We are live on ' + port); });
